ca8227bb66d59a6b91f8956f2b6cb38795165c75
Green Vision Media <113368396+greenvisionmedia@users.noreply.github.com>
2023-04-20 09:03:43 -0600

    use strict

--- src/current-year.js
+++ src/current-year.js
@@ -2,12 +2,12 @@
  * gv-current-year
  * Inserts current year into a <span>
  */
+() => {
+    'use strict';
+    const yearSpan = document.querySelector('.gv-current-year');
 
-()=>{
-  const yearSpan = document.querySelector('.gv-current-year');
-  
-  if (!yearSpan) return;
-  
-  const currentYear = new Date().getFullYear();
-  yearSpan.innerText = currentYear.toString();
+    if (!yearSpan) return;
+
+    const currentYear = new Date().getFullYear();
+    yearSpan.innerText = currentYear.toString();
 };
